2720242343f9e171f7c75e9c36ec8d6181fc407f
jun <ryuu.public@gmail.com>
2025-03-17 02:11:36 +0900

    chore: remove venv from tracking

--- venv/lib/python3.10/site-packages/django_multitenant/apps.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.apps import AppConfig
-
-
-class MultitenantConfig(AppConfig):
-    name = "django_multitenant"
-    verbose_name = "Multitenant"
--- venv/lib/python3.10/site-packages/django_multitenant/db/migrations/__init__.py
+++
@@ -1,1 +0,0 @@
-from .distribute import *
